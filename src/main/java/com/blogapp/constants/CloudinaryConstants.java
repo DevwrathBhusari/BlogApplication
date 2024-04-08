ec1b073d43fd2656d323efa8d76939c02f084525
Devwrath Bhusari <166400312+DevwrathBhusari@users.noreply.github.com>
2024-04-08 21:01:55 +0530

    Update CloudinaryConstants.java

--- src/main/java/com/blogapp/constants/CloudinaryConstants.java
+++ src/main/java/com/blogapp/constants/CloudinaryConstants.java
@@ -1,8 +1,8 @@
 package com.blogapp.constants;
 
 public class CloudinaryConstants {
-	public static final String cloud_name="dqmo1gxgd";
-	public static final String api_key="783265436562189";
-	public static final String api_secret="Ayb-AZUkuj3dMIRn8J4kq8oVWZc";
-	public static final boolean secure=true;
+	public static final String cloud_name="********";
+	public static final String api_key="*************";
+	public static final String api_secret="********************";
+	public static final boolean secure=******;
 }
